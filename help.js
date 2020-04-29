8eee5cec78bd9f8d21e761c4bb4f260fff89abab
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2020-04-29 10:23:08 +0200

    10 and 11

---
+++ help.js
@@ -0,0 +1,113 @@
+/* eslint-disable*/
+
+//* Function
+
+/* function doctorize(firstName) {
+  return `Dr. ${firstName}`;
+} */
+
+//* Anon Function
+
+/* function (firstName) {
+  return `Dr. ${firstName}`;
+} */
+
+//* Function Expression
+
+/* const doctorize = function(firstName) {
+  return `Dr. ${firstName}`;
+}; */
+
+//* Function
+
+/* function inchToCM(inches) {
+  return inches * 2.54;
+} */
+
+//* Function in a variable
+
+/* const inchToCM = function(inches) {
+  return inches * 2.54;
+}; */
+
+//* 'Function' = '=>'
+
+/* const inchToCM = (inches) => {
+  return inches * 2.54;
+}; */
+
+//* Without 'return', function body and parenthesis
+
+/* const inchToCM = inches => inches * 2.54; */
+
+/* function add(a, b = 3) {
+  const total = a + b;
+  return total;
+} */
+
+/* const add = (a, b = 3) => a + b; */
+
+//* Returning a function with an object inside
+
+/* function makeABaby(first, last) {
+  const baby = {
+    name: `${first} ${last}`,
+    age: 0
+  }
+  console.log(typeof baby);
+  return baby;
+} */
+
+/* const makeABaby = (first, last) => ({name: `${first} ${last}`, age: 0});*/
+
+//* IIFE
+//* Immediately Invoked Function Expression
+
+/* (function(age) {
+  console.log('Running the Yolo function', `and you are ${age}`);
+  return 'You are cool and ${age}';
+})(10);
+ */
+
+//* Methods
+
+const umi = {
+  name: 'umi dnv',
+  //! Method
+  sayHi: function () {
+    console.log('Hey Umi');
+    return 'Hey Umi';
+  },
+  //! Short hand Method
+  yellHi() {
+    console.log('HEY UMI')
+  },
+  //! Arrow function
+  whisperHi: () => {
+    console.log('hi umiiiiiiiiiiii')
+  }
+}
+
+//* Callback Functions
+//? Click Callback
+const button = document.querySelector('.clickMe');
+
+function handleClick() {
+  console.log('YOOOOLOOOOO!!');
+}
+
+button.addEventListener('mouseover', function () {
+  console.log('Nice job!!');
+});
+
+//? Timer Callback
+setTimeout(function () {
+  console.log('Done! Time to eat!');
+}, 1000);
+
+// Closure : a function in a function, with private variables
+
+// Callback function : A JavaScript Callback Function is a function that is passed as a parameter to another JavaScript function, and the callback function is run inside of the function it was passed into. Synchronously or asynchronously.
+// For example :
+const hooray = () => console.log('HOORAY !');
+button.addEventListener('click', hooray);
--- log.md
+++ log.md
@@ -73,3 +73,17 @@
 **Today's Progress**: Pursuing Wes Bos' CSSgrid course.
 
 **Thoughts**: It's sunday ! I took time to inline skate and to draw.
+
+
+### Day 10: April 27, 2020
+
+**Today's Progress**: Finally finished my bootstrap challenge !
+
+**Thoughts**: I really prefer CSS vanilla than Bootstrap which is too limited.
+
+
+### Day 11: April 26, 2020
+
+**Today's Progress**: Pursued Wes Bos' JS course and started the Javascript30 challenge. I made the first exercise, the Drum Kit, and completed it with help and researches.
+
+**Thoughts**: It was a rough day, honestly. I cried so much on this exercise ! But I had reassurance from my love one and I made it. I was fully satisfied at the end of the day.
